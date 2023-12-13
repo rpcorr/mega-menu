1103e278f91693548e22469dafafe47e1c371e76
Ronan Corr <ronan_corr@hotmail.com>
2023-12-13 02:19:42 -0500

    show sub menus in mobile view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuMain = document.querySelector('.menu-main');
+
+let subMenu;
+
+menuMain.addEventListener('click', (e) => {
+  if (e.target.closest('.menu-item-has-children')) {
+    const hasChildren = e.target.closest('.menu-item-has-children');
+    showSubMenu(hasChildren);
+  }
+});
+
+function showSubMenu(hasChildren) {
+  subMenu = hasChildren.querySelector('.sub-menu');
+  subMenu.classList.add('active');
+  subMenu.style.animation = 'slideLeft 0.5s ease forwards';
+  const menuTitle =
+    hasChildren.querySelector('i').parentNode.childNodes[0].textContent;
+  menu.querySelector('.current-menu-title').innerHTML = menuTitle;
+  menu.querySelector('.mobile-menu-head').classList.add('active');
+}
