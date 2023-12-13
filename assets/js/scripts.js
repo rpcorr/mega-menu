48b96fa9fb37a0d6f492cde5030e8a96011931bf
Ronan Corr <ronan_corr@hotmail.com>
2023-12-13 15:11:57 -0500

    accessibility - give focus to first sub item when open; loop through sub items using keyboard; close sub menu using keyboard

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -70,7 +70,8 @@ window.onresize = function () {
 
 /* Accessibility */
 var megaMenuLinks = document.querySelectorAll('nav a[href^="#"]');
-console.log(megaMenuLinks.length);
+
+// add eventListener to all the a element in the megaMenu
 for (let i = 0; i < megaMenuLinks.length; i++) {
   megaMenuLinks[i].addEventListener('click', handleLinkClick);
 }
@@ -79,9 +80,71 @@ function handleLinkClick(e) {
   e.preventDefault();
   var target = document.querySelector(`${e.target.getAttribute('href')}`);
 
+  // toggle aria-expanded attribute - this determines if the sub menu is appearing or not
   if (target.getAttribute('aria-expanded') === 'true') {
     target.setAttribute('aria-expanded', 'false');
   } else {
     target.setAttribute('aria-expanded', 'true');
   }
+
+  // store top menu item to give focus later when user closes the sub menu
+  lastFocusedElement = document.activeElement;
+
+  // get the first and last sub menu items to keep users looping through the sub menu items
+  // all the while the sub menu is open
+  var focusableElements = target.querySelectorAll('a');
+  firstTabStop = focusableElements[0];
+  lastTabStop = focusableElements[focusableElements.length - 1];
+
+  // add a timeout for the focus to work
+  setTimeout(() => {
+    firstTabStop.focus();
+  }, 10);
+
+  // add a keyup eventListener for all links
+  var targets = target.querySelectorAll('a');
+  for (var i = 0; i < targets.length; i++) {
+    targets[i].addEventListener('keyup', handleKeypress);
+  }
 }
+
+function handleKeypress(e) {
+  if (e.keyCode === 9 && !e.shiftKey) {
+    // the user is trying to nagivate forward
+    if (document.activeElement === lastTabStop) {
+      e.preventDefault();
+
+      //setTimeout(() => {
+      firstTabStop.focus();
+      //}, 2000);
+    }
+  }
+
+  if (e.keyCode === 9 && e.shiftKey) {
+    // the user is trying to nagivate backwards
+    if (document.activeElement === firstTabStop) {
+      e.preventDefault();
+      setTimeout(() => {
+        lastTabStop.focus();
+      }, 2000);
+    }
+  }
+
+  if (e.keyCode === 27) {
+    // get the sub menu parent element
+    const currentTopMenuItem =
+      document.activeElement.parentElement.closest('.list-item').parentElement;
+
+    // add aria-expaneded = false
+    currentTopMenuItem.setAttribute('aria-expanded', 'false');
+
+    // give sub menu parent element the focus
+    setTimeout(() => {
+      lastFocusedElement.focus();
+    }, 10);
+  }
+}
+
+var lastFocusedElement;
+var firstTabStop;
+var lastTabStop;
